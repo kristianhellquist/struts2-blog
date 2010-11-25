219cfdf8fd2ff5d72ac86a7aafe30bb71b41ff98
Kristian Hellquist <kristian.hellquist@travelpartner.se>
2010-11-25 11:20:33 +0100

    Added BlogsController for REST-api

---
+++ src/com/company/controllers/BlogsController.java
@@ -0,0 +1,44 @@
+package com.company.controllers;
+
+import com.company.models.Blog;
+import com.company.util.BlogManager;
+import com.opensymphony.xwork2.ModelDriven;
+import org.apache.struts2.rest.DefaultHttpHeaders;
+import org.apache.struts2.rest.HttpHeaders;
+import java.util.Collection;
+
+public class BlogsController implements ModelDriven<Object> {
+
+    private BlogManager blogManager = new BlogManager();
+    private String id;
+    private Blog model;
+    private Collection list;
+
+    
+    public Object getModel(){
+        if(list==null){
+            return model;
+        }else{
+            return list;
+        }
+    }
+
+    public HttpHeaders show() {
+        model = blogManager.find(id);
+        return new DefaultHttpHeaders("show");
+    }
+  
+    public HttpHeaders index() {
+        list = blogManager.list();
+        return new DefaultHttpHeaders("show");
+    }
+   
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+}
+
